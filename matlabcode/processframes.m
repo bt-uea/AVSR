3913632e5381e7aa8ce309f51a9a984cd508c4d9
Bruce Tiplady <tippers2000@hotmail.com>
2021-11-27 17:04:43 +0000

    using matlab vision toolbox to detect face region

--- matlabcode/processframes.m
+++ matlabcode/processframes.m
@@ -8,19 +8,20 @@ function processframes(frameFileFilter)
         mkdir(folderPath);
     end
 
+    faceDetector = vision.CascadeObjectDetector();
+
     for i = 1:numel(frameFiles)
         fileName = strcat(frameFiles(i).folder, '\', frameFiles(i).name);
         img = imread(fileName);
         
-        % Remove anything not red
-        img = img - img(:,:,2) - img(:,:,3);
-        
-        % Crop Edges
-        img = cropedges(img);
+        bbox = step(faceDetector, img)
+        bbox=bbox(size(bbox,1),:);
+        img = imcrop(img,bbox);
+        img = img-img(:,:,2)-img(:,:,3);
+        img=img-img(:,:,2)-img(:,:,3);
+        img = img(:,:,1);
         
         imwrite(img, strcat(folderPath, frameFiles(i).name));
-        
-        
     end
 
 end
