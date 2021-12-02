46c696f82e7497326d32eaea90b6b3c02d9a8faf
BenGrumm <ben.grummitt@outlook.com>
2021-12-02 13:13:55 +0000

    Adding some test stuff for lip boundaries in normalize green.
    ycbr test from https://www.atlantis-press.com/article/21943.pdf . Didnt work as well as current temp normalize so sticking with that.  Savitzky outline from https://uk.mathworks.com/matlabcentral/answers/380687-how-to-smooth-rough-edges-along-a-binary-image.

--- matlab/tempNormalizeGreen.m
+++ matlab/tempNormalizeGreen.m
@@ -1,8 +1,18 @@
 function [im] = tempNormalizeGreen(file)
 
-im = imread('mp4s\albi\frames\albi_frame001.png');
+im = imread(file);
+
+% Find face in image as without jacket gives a large highlighted area as
+% well (thanks bruce)
+faceDetector = vision.CascadeObjectDetector();
+bbox = step(faceDetector, im);
+bbox=bbox(size(bbox,1),:);
+im = imcrop(im,bbox);
+
 doubleIm = im2double(im);
 
+% Done after some trial and error normalising RGB then doing mixing the 
+% color spaces for best representation of lips
 R = doubleIm(:, :, 1);
 G = doubleIm(:, :, 2);
 B = doubleIm(:, :, 3);
@@ -13,13 +23,46 @@ im = (R ./ S);
 im = im - (G./ S) * 5;
 im = im + (B./ S) * 5;
 
-% Increase range of temps for more granularity?
-% im = (im .* 200) < 10;
+% This is used to find the average intensity of the lips then threshold for
+% that intensity (todo get an avg value and hardcode)
+P = roipoly(im);
 
-thresh = im > 0.8;
+ptr = find(P);
 
-imshow(thresh);
+M1 = im;
+R = M1(:,:,1);
 
-% imshow(imclose(im > 0.8, strel('disk', 5)));
+cm = mean(R(ptr));
+
+D = sqrt((M1(:,:,1) - cm(1)).^2);
+
+
+L = D < std(D(:));
+
+
+% This will perform an dilation followed by erosion with a structuring
+% elemnt of a disk with radius 9
+closedBin = imclose(L, strel('disk', 9));
+
+% Blur and then rethreshold to remove some rough edges (from
+% https://uk.mathworks.com/matlabcentral/answers/380687-how-to-smooth-rough-edges-along-a-binary-image)
+windowSize = 10;
+kernel = ones(windowSize) / windowSize ^ 2;
+blurryImage = conv2(single(closedBin), kernel, 'same');
+binaryImage = blurryImage > 0.45; % Rethreshold
+
+% Get the boundaries of the bw image
+bound = bwboundaries(binaryImage);
+
+% Display boundaries ontop of original image
+imshow(doubleIm);
+hold on;
+
+numberOfBoundaries = size(bound, 1);
+for k = 1 : numberOfBoundaries
+	thisBoundary = bound{k};
+	plot(thisBoundary(:,2), thisBoundary(:,1), 'g', 'LineWidth', 2);
+end
+hold off;
 
 end
