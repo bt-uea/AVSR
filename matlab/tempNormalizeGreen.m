2e98bd41bbb6530beb73f26031b01bd529631984
BenGrumm <ben.grummitt@outlook.com>
2021-12-02 16:48:30 +0000

    Only draw boundary with inner (doesnt draw with closed lips)

--- matlab/tempNormalizeGreen.m
+++ matlab/tempNormalizeGreen.m
@@ -19,30 +19,23 @@ B = doubleIm(:, :, 3);
 
 S = R + G + B;
 
-im = (R ./ S);
+im = (R ./ S) * 2;
 im = im - (G./ S) * 5;
-im = im + (B./ S) * 5;
+im = im + (B./ S) * 3;
+
 
 % This is used to find the average intensity of the lips then threshold for
 % that intensity (todo get an avg value and hardcode)
-P = roipoly(im);
-
-ptr = find(P);
 
 M1 = im;
-R = M1(:,:,1);
 
-cm = mean(R(ptr));
-
-D = sqrt((M1(:,:,1) - cm(1)).^2);
-
+D = sqrt((M1(:,:,1) - 1.64335881458785).^2);
 
 L = D < std(D(:));
 
-
 % This will perform an dilation followed by erosion with a structuring
 % elemnt of a disk with radius 9
-closedBin = imclose(L, strel('disk', 9));
+closedBin = imclose(L, strel('disk', 15));
 
 % Blur and then rethreshold to remove some rough edges (from
 % https://uk.mathworks.com/matlabcentral/answers/380687-how-to-smooth-rough-edges-along-a-binary-image)
@@ -52,17 +45,21 @@ blurryImage = conv2(single(closedBin), kernel, 'same');
 binaryImage = blurryImage > 0.45; % Rethreshold
 
 % Get the boundaries of the bw image
-bound = bwboundaries(binaryImage);
+% bound = bwboundaries(binaryImage);
 
 % Display boundaries ontop of original image
-imshow(doubleIm);
-hold on;
-
-numberOfBoundaries = size(bound, 1);
-for k = 1 : numberOfBoundaries
-	thisBoundary = bound{k};
-	plot(thisBoundary(:,2), thisBoundary(:,1), 'g', 'LineWidth', 2);
-end
-hold off;
-
+BW = binaryImage;
+[B,L,N] = bwboundaries(BW);
+       figure; 
+       imshow(doubleIm); 
+       hold on;
+       for k = 1:length(B),
+           boundary = B{k};
+           if(k > N)
+               plot(boundary(:,2), boundary(:,1), 'g', 'LineWidth', 2);
+           else
+               plot(boundary(:,2), boundary(:,1), 'r', 'LineWidth', 2);
+           end
+       end
+%}
 end
