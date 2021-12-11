8c8c7c01d9caa16f16aaf87fc6c780f8dd269a22
BenGrumm <ben.grummitt@outlook.com>
2021-12-11 15:25:39 +0000

    Idk whats going on?

--- matlabFinal/BenVisualCode/pcaTesting.m
+++ matlabFinal/BenVisualCode/pcaTesting.m
@@ -1,27 +1,35 @@
 function pcaTesting(struct)
 
-originalImageSize = size(struct(1).cdata);
-
-numPixels = numel(struct(1).cdata);
+imshow(struct(1).cdata);
+percent = 1;
+data = imresize(struct(1).cdata, percent, 'nearest');
+originalImageSize = size(data);
+numPixels = numel(data);
 
 resizedImage = [];
 
 for k = 1:length(struct)
-    newArr = reshape(struct(k).cdata, numPixels, 1);
+    data = imresize(struct(k).cdata, percent, 'nearest');
+    newArr = double(reshape(data, 1, numPixels));
     resizedImage = [resizedImage; newArr];
 end
 
 im_m = mean(resizedImage, 1);
 
-imgP = pca(resizedImage-im_m, 'NumComponents', 15);
+dif = resizedImage-im_m;
 
-imgB = (resizedImage - im_m) * imgP;
+imgP = pca(dif, 'NumComponents', 100);
 
-xim = im_m' + imgP * imgB;
+imgB = dif * imgP;
 
-xim = reshape(xim', originalImageSize);
-figure
-imshow(xim);
+imgPSize = size(imgP)
+
+for i = 1:length(imgB)
+    xim = im_m' + imgP * imgB(i, :)';
+
+    xim = reshape(xim', originalImageSize(1), originalImageSize(2), originalImageSize(3));
+    imshow(xim);
+end
 
 end
 
