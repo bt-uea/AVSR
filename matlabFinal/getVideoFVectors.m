4b7f515c53dd3dcf4b8ac21cbd37479817b3c1c3
Ben <b.grummitt@uea.ac.uk>
2021-12-13 23:04:40 +0000

    Combining Shape & DCT

--- matlabFinal/getVideoFVectors.m
+++ matlabFinal/getVideoFVectors.m
@@ -25,8 +25,8 @@ originalCropVec = zeros(length(videoStruct), numPixels);
 %}
 
 % Num DCT vectors to keep
-numDCT = 100;
-numShape = 0;
+numDCT = 40;
+numShape = 3;
 
 videoVectors = zeros(length(videoStruct), numDCT + numShape);
 
@@ -44,9 +44,9 @@ for frame = 1:length(videoStruct)
     %}
 
     videoVectors(frame, 1:numDCT) = getLipsApplyDCT(rgb2gray(originalCrop), 1, numDCT);
-    % videoVectors(frame, numDCT + 1) = size(lipsOutline, 1);
-    % videoVectors(frame, numDCT + 2) = size(lipsOutline, 2);
-    % videoVectors(frame, numDCT + 3) = sum(binaryLipsCrop, "all");
+    videoVectors(frame, numDCT + 1) = size(lipsOutline, 1);
+    videoVectors(frame, numDCT + 2) = size(lipsOutline, 2);
+    videoVectors(frame, numDCT + 3) = sum(binaryLipsCrop, "all");
     % videoVectors = [videoVectors; tempVec];
     % disp("Loop " + frame);
 end
